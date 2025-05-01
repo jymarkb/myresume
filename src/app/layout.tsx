47389c8923132536018410e07a656d4442a19dd6
arcobaleno <jymark.borja@gmail.com>
2025-05-01 21:48:07 +0800

    update: layout template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <header>
+      <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      </header>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} `}>
         {children}
       </body>
