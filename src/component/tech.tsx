4c2909d7b4d5e14ae00fa8df47f9e3c5d9dd503b
arcobaleno <jymark.borja@gmail.com>
2025-05-02 05:15:40 +0800

    update: tech stack component

--- src/component/tech.tsx
+++ src/component/tech.tsx
@@ -13,34 +13,37 @@ const Tech = () => {
   };
 
   return (
-    <div>
-      <div className="tech-buttons grid grid-cols-4 font-semi-bold gap-8">
+    <>
+      <div className="tech-buttons grid grid-cols-4 font-semi-bold sm:gap-4 gap-4 md:gap-8 lg:gap-4 xl:gap-8 text-xs md:text-base">
         <TechButton selectedId={selectedButton} onSelectBtn={setSelected} />
       </div>
 
-      <div className="grid grid-cols-5 py-8 gap-8">
+      <div className="grid grid-cols-3 sm:grid-cols-5 py-8 gap-4 md:gap-8 lg:gap-4 xl:gap-8">
         {techData[selectedButton].items.map((item, index) => {
           const uniqueKey = `${selectedButton}-${index}`;
           return (
             <div
               key={uniqueKey}
-              className="tech-image h-28 w-28 bg-gray-200 rounded flex flex-col justify-center items-center text-center p-1 gap-1"
+              className="tech-image aspect-square bg-gray-200 rounded flex flex-col justify-center items-center text-center gap-1"
               style={{ animationDelay: `${index * 0.03}s` }}
             >
-              <Image
-                loading="lazy"
-                src={`${techDataPath}/${item.imgSrc}`}
-                className="h-16 w-16 object-contain bg-gray-200 "
-                alt="technology image"
-                height={64}
-                width={64}
-              />
-              <p className="text-sm font-semibold">{item.name}</p>
+              <div className="h-10 w-10 md:h-12 md:w-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16 aspect-square flex items-center">
+                <Image
+                  loading="lazy"
+                  src={`${techDataPath}/${item.imgSrc}`}
+                  className=" object-cover bg-gray-200"
+                  alt="technology image"
+                  height={64}
+                  width={64}
+                />
+              </div>
+
+              <p className="text-xs md:text-sm font-semibold">{item.name}</p>
             </div>
           );
         })}
       </div>
-    </div>
+    </>
   );
 };
 
