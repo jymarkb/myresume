817759064726bd3d4c7ce9e08aa1164cb2d45bfa
arcobaleno <jymark.borja@gmail.com>
2025-05-06 06:01:20 +0800

    update: work section component

--- src/component/overflow-description.tsx
+++ src/component/overflow-description.tsx
@@ -1,46 +1,40 @@
-import ButtonExpandImage from "./expand-button";
-const OverflowDescription = () => {
+"use client";
+import { useEffect, useState } from "react";
+import ExpandedGallery from "./expand-gallery";
+import FeatureDescription from "./feature-description";
+
+const OverflowDescription = ({ imageId }: { imageId: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedImgId, setSelectedImgId] = useState("");
+
+  const onShowGallery = () => {
+    document.body.style.overflow = "";
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setSelectedImgId(imageId);
+  }, [imageId]);
+
   return (
-    <div
-      className="absolute top-0 right-0 h-full w-1/3 bg-gray-800/95 text-white 
-          opacity-0 group-hover:opacity-100 
-          transition-all duration-300 
-          "
-    >
-      <div className="flex justify-end p-2">
-        <ButtonExpandImage buttonId="0" />
-      </div>
-      <div className="py-16 px-8 flex flex-col justify-center gap-4">
-        <h1 className="font-bold text-primaryTheme text-2xl">
-          {"The </blog> Blog"}
-        </h1>
-        <p className="text-sm">
-          The {"</blog>"} landing page is a sleek, responsive entry point to my
-          custom CMS, built with Symfony 7, React 18, and Tailwind CSS. It
-          features dynamic content, smooth interactions, and a clean layout for
-          showcasing blog posts.
-        </p>
-        <div className="flex gap-4 items-center mt-4">
-          <a
-            href="https://github.com/jymarkb/symfonyBlog"
-            aria-label="github repository link"
-            className="text-xl"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="rounded-full border border-white p-2 h-8 w-8 icon-github transition duration-300 hover:bg-primary"></i>
-          </a>
-          <a
-            href="https://myblog.jymarkb.cloud/"
-            className="flex items-center gap-2 border border-white w-fit px-4 py-2 rounded-sm transition duration-300 hover:bg-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Visit Page <i className="icon-arrow-right"></i>
-          </a>
+    <>
+      <div
+        className={`absolute top-0 right-0 ${imageId === "0" ? "h-full w-[60%] sm:w-1/2 lg:w-[40%]" : "w-fit h-fit"} bg-gray-800/95 text-white opacity-0 group-hover:opacity-100 transition-all duration-300 `}
+      >
+        <div className="flex justify-end p-2">
+          <button className="h-6 w-6 sm:h-8 sm:w-8" onClick={onShowGallery}>
+            <i className="h-6 w-6 sm:h-8 sm:w-8 text-xl icon-expand"></i>
+          </button>
         </div>
+        {imageId === "0" ? <FeatureDescription /> : ""}
       </div>
-    </div>
+
+      {/* gallery here */}
+      {isOpen && (
+        <ExpandedGallery selectedImg={selectedImgId} onClose={onShowGallery} />
+      )}
+    </>
   );
 };
+
 export default OverflowDescription;
