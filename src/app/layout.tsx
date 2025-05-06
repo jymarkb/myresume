30848577f9c16b9a5ae125a1ebd86092cc9f283b
arcobaleno <jymark.borja@gmail.com>
2025-05-06 09:43:53 +0800

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="icon" href="images/mobile-logo.svg" type="image/icon type" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} `}>
         {children}
