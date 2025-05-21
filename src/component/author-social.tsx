e0fe74aea3020d29784eb369807e95e37340fd64
arcobaleno <jymark.borja@gmail.com>
2025-05-21 18:06:45 +0800

    update: target work target link

--- src/component/author-social.tsx
+++ src/component/author-social.tsx
@@ -1,16 +1,17 @@
 import GetInTouch from "./get-in-touch";
 import Socials from "./socials";
+import TargetLink from "./target-link";
 const AuthorSocial = () => {
   return (
     <>
       <div className="flex gap-4">
         <GetInTouch />
-        <a
-          className="px-5 sm:px-8 py-2 text-primary border rounded shadow-md transition duration-300 hover:scale-105"
-          href="/projects"
+        <TargetLink
+          style="px-5 sm:px-8 py-2 text-primary border rounded shadow-md transition duration-300 hover:scale-105"
+          target="#projects"
         >
           See my work
-        </a>
+        </TargetLink>
       </div>
       <ul className="flex font-thin gap-4 items-center sm:ml-auto md:ml-0 lg:ml-auto">
         <Socials />
